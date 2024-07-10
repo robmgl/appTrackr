4cdf01d31c6c9afb1aaf6a566d90b1b61cfc292c
Rob Miguel <robmiguel2@gmail.com>
2024-07-10 16:54:19 -0600

    added ability to edit job

---
+++ JobTracker/EditJobView.swift
@@ -0,0 +1,18 @@
+//
+//  EditJobView.swift
+//  JobTracker
+//
+//  Created by Rob Miguel on 7/10/24.
+//
+
+import SwiftUI
+
+struct EditJobView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    EditJobView()
+}
