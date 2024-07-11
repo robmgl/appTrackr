ed9306faf9a195b155f52fcc38e85a5244afd6a0
Rob Miguel <robmiguel2@gmail.com>
2024-07-10 21:40:15 -0600

    added jobcardview

---
+++ JobTracker/JobCardView.swift
@@ -0,0 +1,18 @@
+//
+//  JobCardView.swift
+//  JobTracker
+//
+//  Created by Rob Miguel on 7/10/24.
+//
+
+import SwiftUI
+
+struct JobCardView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    JobCardView()
+}
